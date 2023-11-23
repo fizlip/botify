085167f2db7ced169edda0c8cff949ae5d3846ec
fizlip <fillatino@gmail.com>
2023-11-23 22:10:28 +0100

    first commit

---
+++ packages/hardhat/contracts/MyNFT.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    constructor(address owner) ERC721("myNFT", "NFT") {
+        _transferOwnership(owner);
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
